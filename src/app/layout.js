4e9b3012f4d939b47a4adaf4e33c29a9af343c31
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-06-29 21:15:18 +0600

    fixed layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,19 +20,20 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "LegalEase - Connect with Verified Legal Experts",
-  description:
-    "Find, consult, and hire top-rated lawyers and attorneys across all practice areas. Your legal solutions made simple with LegalEase.",
+  description: "Find, consult, and hire top-rated lawyers and attorneys across all practice areas. Your legal solutions made simple with LegalEase.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
-      suppressHydrationWarning
+      lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <main className="flex-1">{children}</main>
+        <NextThemeProvider> <Navbar></Navbar>
+          <main className="flex-1">{children}</main>
+          <Footer></Footer>
+          <Toaster position="top-right" /></NextThemeProvider>
       </body>
     </html>
   );
